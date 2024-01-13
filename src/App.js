27548dea9abe988f2ea043a44f9fbfac1f0a3c8e
ritik <ritikbhatt020@gmail.com>
2024-01-14 00:55:41 +0530

    Added routes to the application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Movies from "./components/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import React from 'react'
 
 function App() {
   return (
-    <main className="container">
-      <Movies></Movies>
-    </main>
+    <React.Fragment>
+      <NavBar></NavBar>
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies"></Redirect>
+          <Redirect to="not-found"></Redirect>
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
